1fe4abb084f8e57a8d355b99e7e49572708ced56
thriveni chowdary <thrivenidamancharla@gmail.com>
2025-11-20 22:36:40 +0530

    Create 08_trie.py

---
+++ 08_trie.py
@@ -0,0 +1,40 @@
+# Trie (Prefix Tree) Implementation
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+    
+    def search(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end_of_word
+    
+    def starts_with(self, prefix):
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+if __name__ == '__main__':
+    trie = Trie()
+    trie.insert('apple')
+    trie.insert('app')
+    print(trie.search('apple'))
+    print(trie.starts_with('ap'))
