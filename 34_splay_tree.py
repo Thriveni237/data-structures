f26d9ec55382c67c60475b6b9164b8f9bb96eb4f
thriveni chowdary <thrivenidamancharla@gmail.com>
2025-11-20 22:53:42 +0530

    Create 34_splay_tree.py

---
+++ 34_splay_tree.py
@@ -0,0 +1,8 @@
+# Splay Tree
+class SplayNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+root = SplayNode(10)
+print('Splay Tree created')
