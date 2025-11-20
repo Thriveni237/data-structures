d383c42844b67f59cd4a1487c3cf9f70b0f34452
thriveni chowdary <thrivenidamancharla@gmail.com>
2025-11-20 22:28:20 +0530

    Create 04_binary_search_tree.py

---
+++ 04_binary_search_tree.py
@@ -0,0 +1,71 @@
+# Binary Search Tree Implementation
+
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+    
+    def insert(self, value):
+        """Insert a value into the BST"""
+        if self.root is None:
+            self.root = TreeNode(value)
+        else:
+            self._insert_recursive(self.root, value)
+    
+    def _insert_recursive(self, node, value):
+        if value < node.value:
+            if node.left is None:
+                node.left = TreeNode(value)
+            else:
+                self._insert_recursive(node.left, value)
+        else:
+            if node.right is None:
+                node.right = TreeNode(value)
+            else:
+                self._insert_recursive(node.right, value)
+    
+    def search(self, value):
+        """Search for a value in the BST"""
+        return self._search_recursive(self.root, value)
+    
+    def _search_recursive(self, node, value):
+        if node is None:
+            return False
+        if node.value == value:
+            return True
+        elif value < node.value:
+            return self._search_recursive(node.left, value)
+        else:
+            return self._search_recursive(node.right, value)
+    
+    def inorder_traversal(self):
+        """Inorder traversal (Left, Root, Right)"""
+        result = []
+        self._inorder_recursive(self.root, result)
+        return result
+    
+    def _inorder_recursive(self, node, result):
+        if node is not None:
+            self._inorder_recursive(node.left, result)
+            result.append(node.value)
+            self._inorder_recursive(node.right, result)
+
+# Test the BST
+if __name__ == '__main__':
+    bst = BinarySearchTree()
+    bst.insert(50)
+    bst.insert(30)
+    bst.insert(70)
+    bst.insert(20)
+    bst.insert(40)
+    bst.insert(60)
+    bst.insert(80)
+    
+    print('Inorder Traversal:', bst.inorder_traversal())
+    print('Search 40:', bst.search(40))
+    print('Search 100:', bst.search(100))
