ec5bd15a59b20d217880e9b746b80c2427875096
thriveni chowdary <thrivenidamancharla@gmail.com>
2025-11-20 22:46:53 +0530

    Create 20_fenwick_tree.py

---
+++ 20_fenwick_tree.py
@@ -0,0 +1,18 @@
+# Fenwick Tree
+class FenwickTree:
+    def __init__(self, n):
+        self.n = n
+        self.tree = [0] * (n + 1)
+    def update(self, i, delta):
+        while i <= self.n:
+            self.tree[i] += delta
+            i += i & (-i)
+    def query(self, i):
+        s = 0
+        while i > 0:
+            s += self.tree[i]
+            i -= i & (-i)
+        return s
+ft = FenwickTree(5)
+ft.update(1, 3)
+print('Fenwick Tree created')
