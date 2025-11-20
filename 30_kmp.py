daa2bb95a25f91e3ca3abdc475c7240f3cc609db
thriveni chowdary <thrivenidamancharla@gmail.com>
2025-11-20 22:51:58 +0530

    Create 30_kmp.py

---
+++ 30_kmp.py
@@ -0,0 +1,13 @@
+# KMP Algorithm
+def build_lps(pattern):
+    lps = [0] * len(pattern)
+    j = 0
+    for i in range(1, len(pattern)):
+        while j > 0 and pattern[i] != pattern[j]:
+            j = lps[j - 1]
+        if pattern[i] == pattern[j]:
+            j += 1
+        lps[i] = j
+    return lps
+lps = build_lps('ABABC')
+print('KMP created')
